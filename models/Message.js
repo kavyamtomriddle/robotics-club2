757cc2b98e02e0768c7768984153296693e44601
hitkavyam <hitkavyam1434@gmail.com>
2025-07-11 02:26:25 +0530

    Initial backend for contact form

---
+++ models/Message.js
@@ -0,0 +1,12 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  subject: String,
+  message: String,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
